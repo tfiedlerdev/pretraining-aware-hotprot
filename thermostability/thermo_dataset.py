4d7d51e6aac502a0c6176cfa449860a75518ba77
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-01-06 12:33:47 +0100

    WIP attempt to run training

--- thermostability/thermo_dataset.py
+++ thermostability/thermo_dataset.py
@@ -1,34 +1,54 @@
 from torch.utils.data import Dataset
 import pandas as pd
 import torch
+import os
+import pickle
+from IPython.display import clear_output, display
 
 class ThermostabilityDataset(Dataset):
-    def __init__(self, file_path) -> None:
+    def __init__(self, file_path: str, use_cache: bool = True) -> None:
         super().__init__()
         sequences = []
         melting_points = []
-        with open(file_path) as file:
-            for line in file:
-                line_tokens = line.split(' ')
-                id = line_tokens[0]
-                melting_point = float(line_tokens[1].split('=')[1])
-                sequence = line_tokens[2]
 
-                melting_points.append(melting_point)
-                sequences.append(sequence)
+        cache_file = file_path+"_cache.p"
+
+        if use_cache and os.path.exists(cache_file):
+            print("Loading data from cache file: ", cache_file)
+            with open( cache_file, "rb" ) as f:
+                self.thermo_dataframe = pickle.load(f)
+        else:     
+            with open(file_path) as file:
+                for index, line in enumerate(file):
+                    if index%50 == 0:
+                        clear_output(wait=True)
+                        print(f"Reading line {index}", flush=True)
+                    if line.startswith(">"):
+                        line_tokens = line.split(' ')
+                        id = line_tokens[0]
+                        melting_point = float(line_tokens[1].split('=')[1])
+                        melting_points.append(melting_point)
+                    else: 
+                        sequence = line.replace("\n", "")
+                        sequences.append(sequence)
             
-        data = {'x': sequences, 'y': melting_points}
+            data = {'x': sequences, 'y': melting_points}
 
-        self.thermo_dataframe = pd.DataFrame(data)
+            self.thermo_dataframe = pd.DataFrame(data)
+            if use_cache:
+                with open( cache_file, "wb" ) as f:
+                    pickle.dump(self.thermo_dataframe, f)
 
     def __len__(self):
-        return len(self.thermo_dataframe)
+        return 20#len(self.thermo_dataframe)
 
     def __getitem__(self, index):
         if(torch.is_tensor(index)):
             index = index.tolist()
 
         items = self.thermo_dataframe.iloc[index, :]
-        return items
+        
+
+        return items["x"], torch.tensor([items["y"]], dtype=torch.float32)
 
         
