1d9c94a060d80304628fa7db34eba689fe09ccb7
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-02-08 09:10:03 +0100

    WIP work on supporting multiple input files

--- thermostability/hotinfer.py
+++ thermostability/hotinfer.py
@@ -2,29 +2,35 @@ from torch import nn
 import torch
 import esm
 from typing import List
+from esmfold_custom import esm as esm_custom
+import os
+import pickle
+
 
 class HotInfer(nn.Module):
     def __init__(
-        self, 
-        rnn_hidden_size=128, 
-        rnn_hidden_layers=2, 
-        nonlinearity_rnn='relu',
+        self,
+        rnn_hidden_size=128,
+        rnn_hidden_layers=2,
+        nonlinearity_rnn="relu",
         nonlinearity_thermomodule=nn.ReLU(),
     ):
         super().__init__()
-        #self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.esmfold = esm.pretrained.esmfold_v1()
         # cfg = self.esm.cfg
         # s_s shape torch.Size([1, sequence_len, 1024])
         # s_z shape torch.Size([1, sequence_len, sequence_len, 128])
 
-        self.thermo_module_rnn = torch.nn.RNN(input_size=1024,
-            hidden_size =rnn_hidden_size, 
-            num_layers =rnn_hidden_layers, 
-            nonlinearity=nonlinearity_rnn, 
-            batch_first =True,
-            bidirectional=False)
-        
+        self.thermo_module_rnn = torch.nn.RNN(
+            input_size=1024,
+            hidden_size=rnn_hidden_size,
+            num_layers=rnn_hidden_layers,
+            nonlinearity=nonlinearity_rnn,
+            batch_first=True,
+            bidirectional=False,
+        )
+
         self.thermo_module_regression = torch.nn.Sequential(
             nn.Flatten(),
             nn.LayerNorm(rnn_hidden_layers * rnn_hidden_size),
@@ -34,46 +40,50 @@ class HotInfer(nn.Module):
             nonlinearity_thermomodule,
             nn.Linear(64, 16),
             nonlinearity_thermomodule,
-            nn.Linear(16,1))
-            
-        #self.thermo_module_rnn = thermo_module_rnn.to(self.device)
-        #self.thermo_module_regression = thermo_module_regression.to(self.device)
-      
+            nn.Linear(16, 1),
+        )
 
-    def forward(self,
-       sequences: List[str]
-        ):
+        # self.thermo_module_rnn = thermo_module_rnn.to(self.device)
+        # self.thermo_module_regression = thermo_module_regression.to(self.device)
+
+    def forward(self, sequences: List[str]):
         with torch.no_grad():
             esm_output = self.esmfold.infer(sequences=sequences)
             s_s = esm_output["s_s"]
 
         _, rnn_hidden = self.thermo_module_rnn(s_s)
-        thermostability = self.thermo_module_regression(torch.transpose(rnn_hidden, 0,1))
-    
+        thermostability = self.thermo_module_regression(
+            torch.transpose(rnn_hidden, 0, 1)
+        )
+
         return thermostability
-        
+
+
 class HotInferModelParallel(nn.Module):
     def __init__(
-        self, 
-        rnn_hidden_size=128, 
-        rnn_hidden_layers=2, 
-        nonlinearity_rnn='relu',
+        self,
+        rnn_hidden_size=128,
+        rnn_hidden_layers=2,
+        nonlinearity_rnn="relu",
         nonlinearity_thermomodule=nn.ReLU(),
+        representation_key="s_s_0",
     ):
         super().__init__()
-        #self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.esmfold = esm.pretrained.esmfold_v1().to("cuda:0")
         # cfg = self.esm.cfg
         # s_s shape torch.Size([1, sequence_len, 1024])
         # s_z shape torch.Size([1, sequence_len, sequence_len, 128])
 
-        self.thermo_module_rnn = torch.nn.RNN(input_size=1024,
-            hidden_size =rnn_hidden_size, 
-            num_layers =rnn_hidden_layers, 
-            nonlinearity=nonlinearity_rnn, 
-            batch_first =True,
-            bidirectional=False).to("cuda:1")
-        
+        self.thermo_module_rnn = torch.nn.RNN(
+            input_size=1024,
+            hidden_size=rnn_hidden_size,
+            num_layers=rnn_hidden_layers,
+            nonlinearity=nonlinearity_rnn,
+            batch_first=True,
+            bidirectional=False,
+        ).to("cuda:1")
+
         self.thermo_module_regression = torch.nn.Sequential(
             nn.Flatten(),
             nn.LayerNorm(rnn_hidden_layers * rnn_hidden_size),
@@ -83,20 +93,35 @@ class HotInferModelParallel(nn.Module):
             nonlinearity_thermomodule,
             nn.Linear(64, 16),
             nonlinearity_thermomodule,
-            nn.Linear(16,1)).to("cuda:1")
-            
-        #self.thermo_module_rnn = thermo_module_rnn.to(self.device)
-        #self.thermo_module_regression = thermo_module_regression.to(self.device)
-      
+            nn.Linear(16, 1),
+        ).to("cuda:1")
 
-    def forward(self,
-       sequences: List[str]
-        ):
+        self.representation_key = representation_key
+        self.meta_filepath = f"data/model_parallel_{representation_key}_meta.pickle"
+        if os.path.exists(self.meta_filepath):
+            with open(self.meta_filepath) as f:
+                self.meta = pickle.load(f)
+        else:
+            self.meta = {}
+        # self.thermo_module_rnn = thermo_module_rnn.to(self.device)
+        # self.thermo_module_regression = thermo_module_regression.to(self.device)
+
+    def forward(self, sequences: List[str]):
+        # TODO: optimize (run esmfold for samples while thermomodule runs, replace esmfold by esm2)
         with torch.no_grad():
-            esm_output = self.esmfold.infer(sequences=sequences)
-            s_s_0 = esm_output["s_s_0"].to("cuda:1")
+            reprs = [
+                torch.load(
+                    os.path.join("data", self.representation_key, self.meta[seq])
+                )
+                if seq in self.meta
+                else self.esmfold.infer(sequences=[seq])[self.representation_key].squeeze()
+                for seq in sequences
+            ]
+            reprBatch = torch.stack(reprs).to("cuda:1")
 
-        _, rnn_hidden = self.thermo_module_rnn(s_s_0)
-        thermostability = self.thermo_module_regression(torch.transpose(rnn_hidden, 0,1))
-    
+        _, rnn_hidden = self.thermo_module_rnn(reprBatch)
+        thermostability = self.thermo_module_regression(
+            torch.transpose(rnn_hidden, 0, 1)
+        )
+
         return thermostability
---
+++ util/train.py
@@ -0,0 +1,154 @@
+import torch
+import time
+import copy
+from torch import nn as nn
+from tqdm.notebook import tqdm
+import sys
+import wandb
+import pylab as pl
+from typing import Callable
+
+
+def train_model(
+    model,
+    optimizer: torch.optim.Optimizer,
+    criterion,
+    scheduler,
+    dataloaders,
+    dataset_sizes,
+    use_wandb,
+    num_epochs=25,
+    return_best_model=True,
+    max_gradient_clip: float = 10,
+    prepare_inputs: Callable[[torch.Tensor], torch.Tensor] = lambda x: x,
+    prepare_labels: Callable[[torch.Tensor], torch.Tensor] = lambda x: x,
+):
+    since = time.time()
+    best_model_path = "results/best_model.pt"
+    if return_best_model:
+        torch.save(model, best_model_path)
+
+    best_epoch_loss = sys.float_info.max
+    losses = []
+    batchEnumeration = []
+    bestEpochPredictions = torch.tensor([])
+    bestEpochLabels = torch.tensor([])
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch}/{num_epochs - 1}")
+        print("-" * 10)
+        currentEpochPredictions = torch.tensor([])
+        currentEpochLabels = torch.tensor([])
+        # Each epoch has a training and validation phase
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+
+            # Iterate over data.
+            for idx, (inputs, labels) in enumerate(dataloaders[phase]):
+                inputs = prepare_inputs(inputs)
+                labels = prepare_labels(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    loss = criterion(outputs, torch.unsqueeze(labels, 1))
+
+                    if phase == "val":
+                        currentEpochPredictions = torch.cat(
+                            (currentEpochPredictions, outputs.cpu())
+                        )
+                        currentEpochLabels = torch.cat(
+                            (currentEpochLabels, labels.cpu())
+                        )
+                    # backward + optimize only if in training phase
+                    if phase == "train":
+
+                        if not torch.isnan(loss):
+                            loss.backward()
+                            if max_gradient_clip:
+                                threshold = max_gradient_clip
+                                for p in model.parameters():
+                                    if p.grad != None:
+                                        if p.grad.norm() > threshold:
+                                            torch.nn.utils.clip_grad_norm_(p, threshold)
+                            optimizer.step()
+                        if torch.isnan(loss).any():
+                            print(
+                                f"Nan loss: {torch.isnan(loss)}| Loss: {loss}| inputs: {inputs}"
+                            )
+                # statistics
+                batch_size = inputs.size(0)
+                batch_loss = loss.item() * batch_size
+                losses.append(batch_loss)
+                batchEnumeration.append(
+                    batchEnumeration[-1] + 1 if len(batchEnumeration) > 0 else 0
+                )
+
+                running_loss += batch_loss
+                mean_abs_diff = (
+                    torch.abs(outputs.squeeze().sub(labels.squeeze()))
+                    .squeeze()
+                    .mean()
+                    .item()
+                )
+                if use_wandb:
+                    wandb.log({"mean_abs_diff": mean_abs_diff})
+                if idx % 1 == 0:
+                    tqdm.write(
+                        "Epoch: [{}/{}], Batch: [{}/{}], loss: {:.6f}, batch abs diff mean {:.6f}".format(
+                            epoch,
+                            num_epochs,
+                            idx + 1,
+                            len(dataloaders[phase]),
+                            batch_loss / float(batch_size),
+                            mean_abs_diff,
+                        ),
+                        end="\r",
+                    )
+
+            if phase == "train":
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+
+            print(f"{phase} Loss: {epoch_loss:.4f}")
+
+            # deep copy the model
+            if phase == "val" and epoch_loss < best_epoch_loss:
+                best_epoch_loss = epoch_loss
+                if return_best_model:
+                    torch.save(model, best_model_path)
+                if use_wandb:
+                    wandb.log({"mse_loss": epoch_loss})
+                bestEpochLabels = currentEpochLabels
+                bestEpochPredictions = currentEpochPredictions
+        print()
+
+    time_elapsed = time.time() - since
+    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
+    print(f"Best val Acc: {best_epoch_loss:4f}")
+    # load best model weights
+    if return_best_model:
+        model = torch.load(best_model_path)
+
+    pl.scatter(
+        bestEpochPredictions.squeeze().tolist(), bestEpochLabels.squeeze().tolist()
+    )
+    plotPath = f"results/predictions.png"
+    pl.title(f"Loss: {best_epoch_loss}")
+    pl.xlabel("Predictions")
+    pl.ylabel("Labels")
+    pl.savefig(plotPath)
+    if use_wandb:
+        artifact = wandb.Artifact("results", type="train")
+        artifact.add_file(plotPath)
+        wandb.log_artifact(artifact)
+    return model, best_epoch_loss
