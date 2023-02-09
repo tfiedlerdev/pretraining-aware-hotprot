c8fc3c41fe6e7c9be7e88e9aadc8e9fafb18ce69
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-02-09 09:38:14 +0100

    Replace train_model by import

--- hyperparameters.py
+++ hyperparameters.py
@@ -28,135 +28,7 @@ if torch.cuda.is_available():
 cpu = torch.device("cpu")
 torch.cuda.empty_cache()
 torch.cuda.list_gpu_processes()
-
-
-def train_model(
-    model, optimizer, criterion, scheduler, dataloaders, dataset_sizes, use_wandb, num_epochs=25
-):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-
-    best_epoch_loss = sys.float_info.max
-    losses = []
-    batchEnumeration = []
-    bestEpochPredictions = torch.tensor([])
-    bestEpochLabels = torch.tensor([])
-    for epoch in range(num_epochs):
-        print(f"Epoch {epoch}/{num_epochs - 1}")
-        print("-" * 10)
-        currentEpochPredictions = torch.tensor([])
-        currentEpochLabels = torch.tensor([])
-        # Each epoch has a training and validation phase
-        for phase in ["train", "val"]:
-            if phase == "train":
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
-
-            running_loss = 0.0
-
-            # Iterate over data.
-            for idx, (inputs, labels) in enumerate(dataloaders[phase]):
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = model(inputs)
-                    loss = criterion(outputs, torch.unsqueeze(labels, 1))
-
-                    if phase == "val":
-                        currentEpochPredictions = torch.cat(
-                            (currentEpochPredictions, outputs.cpu())
-                        )
-                        currentEpochLabels = torch.cat((currentEpochLabels, labels.cpu()))
-                    # backward + optimize only if in training phase
-                    if phase == "train":
-
-                        if not torch.isnan(loss):
-                            loss.backward()
-                            threshold = 10
-                            for p in model.parameters():
-                                if p.grad != None:
-                                    if p.grad.norm() > threshold:
-                                        torch.nn.utils.clip_grad_norm_(p, threshold)
-                            optimizer.step()
-                        if torch.isnan(loss).any():
-                            print(
-                                f"Nan loss: {torch.isnan(loss)}| Loss: {loss}| inputs: {inputs}"
-                            )
-                # statistics
-                batch_size = inputs.size(0)
-                batch_loss = loss.item() * batch_size
-                losses.append(batch_loss)
-                batchEnumeration.append(
-                    batchEnumeration[-1] + 1 if len(batchEnumeration) > 0 else 0
-                )
-
-                running_loss += batch_loss
-                mean_abs_diff = (
-                    torch.abs(outputs.squeeze().sub(labels.squeeze()))
-                    .squeeze()
-                    .mean()
-                    .item()
-                )
-                if use_wandb:
-                    wandb.log({"mean_abs_diff": mean_abs_diff})
-                if idx % 1 == 0:
-                    tqdm.write(
-                        "Epoch: [{}/{}], Batch: [{}/{}], loss: {:.6f}, batch abs diff mean {:.6f}".format(
-                            epoch,
-                            num_epochs,
-                            idx + 1,
-                            len(dataloaders[phase]),
-                            batch_loss / float(batch_size),
-                            mean_abs_diff,
-                        ),
-                        end="\r",
-                    )
-
-            if phase == "train":
-                scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-
-            print(f"{phase} Loss: {epoch_loss:.4f}")
-
-            # deep copy the model
-            if phase == "val" and epoch_loss < best_epoch_loss:
-                best_epoch_loss = epoch_loss
-                best_model_wts = copy.deepcopy(model.state_dict())
-                if use_wandb:
-                    wandb.log({"mse_loss": epoch_loss})
-                bestEpochLabels = currentEpochLabels
-                bestEpochPredictions = currentEpochPredictions
-        print()
-
-    time_elapsed = time.time() - since
-    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s")
-    print(f"Best val Acc: {best_epoch_loss:4f}")
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    
-    pl.scatter(
-        bestEpochPredictions.squeeze().tolist(),
-        bestEpochLabels.squeeze().tolist()
-    )
-    plotPath = f"results/predictions.png"
-    pl.title(f"Loss: {best_epoch_loss}")
-    pl.xlabel("Predictions")
-    pl.ylabel("Labels")
-    pl.savefig(plotPath)
-    if use_wandb:
-        artifact = wandb.Artifact("results", type="train")
-        artifact.add_file(plotPath)
-        wandb.log_artifact(artifact)
-    return model, best_epoch_loss
+from util.train import train_model
 
 
 def run_train_experiment(config: dict = None, use_wandb = True):
@@ -210,13 +82,13 @@ def run_train_experiment(config: dict = None, use_wandb = True):
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.5)
     model, score = train_model(
         model,
-        optimizer_ft,
         criterion,
         exp_lr_scheduler,
         dataloaders,
         dataset_sizes,
         use_wandb,
         num_epochs=config["epochs"],
+        prepare_labels=lambda x: x.to("cuda:0")
     )
     return score
 
