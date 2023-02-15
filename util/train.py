Fix mean absolute diff calculation

util/train.py
@@ -101,7 +101,7 @@ def train_model(
                 batchEnumeration.append(
                     batchEnumeration[-1] + 1 if len(batchEnumeration) > 0 else 0
                 )
-                num_batches= dataset_sizes[phase] / batch_size
+                
                 running_loss += batch_loss
                 mean_abs_diff = (
                     torch.abs(outputs.squeeze().sub(labels.squeeze()))
@@ -110,7 +110,7 @@ def train_model(
                     .item()
                 )
                
-                epoch_mad += mean_abs_diff / num_batches
+                epoch_mad += mean_abs_diff
                 if use_wandb:
                     wandb.log({"mean_abs_diff": mean_abs_diff})
                 if idx % 1 == 0:
@@ -121,7 +121,7 @@ def train_model(
                             idx + 1,
                             len(dataloaders[phase]),
                             batch_loss / float(batch_size),
-                            epoch_mad,
+                            epoch_mad/(idx+1),
                         ),
                         end="\r",
                     )