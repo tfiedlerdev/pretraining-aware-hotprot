c92ecd3632669e627a713ce88c21333380fc9cf9
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-02-17 18:20:42 +0100

    Add MAD history plot

--- util/experiments.py
+++ util/experiments.py
@@ -11,25 +11,27 @@ def store_experiment(
     epoch_predictions: "list[float]",
     epoch_actuals: "list[float]",
     args: dict,
-    epoch_losses: "dict[Literal['train', 'val'], list[float]]" = None,
+    epoch_mads: "dict[Literal['train', 'val'], list[float]]" = None,
 ):
     plot_predictions(
         f"predictions",
         f"Loss: {epoch_loss: .2f}, mad {epoch_mad: .2f}",
         epoch_predictions,
         epoch_actuals,
-        False,
         output_dir=output_dir_path,
     )
     with open(os.path.join(output_dir_path, "metadata.json"), "w") as f:
         json.dump(args, f, indent=5)
 
-    if epoch_losses:
-        for key, values in epoch_losses.items():
+    if epoch_mads:
+        plt.clf()
+        
+        for values in epoch_mads.values():
             plt.plot(range(len(values)), values)
-            plt.title(f"Loss over {key} epochs")
-            plt.xlabel("Epoch")
-            plt.ylabel("Loss ")
-            plt.savefig(os.path.join(output_dir_path, f"losses_{key}.png"))
+        plt.xlabel("Epoch")
+        plt.ylabel("MAD ")
+        plt.title(f"MAD over epochs")
+        plt.legend(epoch_mads.keys())    
+        plt.savefig(os.path.join(output_dir_path, f"mads.png"))
 
     print(f"Results stored in {output_dir_path}")
