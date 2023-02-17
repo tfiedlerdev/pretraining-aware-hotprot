541adb5b5215ff1710c1b8343bf4039404f729f5
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-02-17 18:17:49 +0100

    Generify fc model

--- applications/train.py
+++ applications/train.py
@@ -19,8 +19,7 @@ from thermostability.thermo_pregenerated_dataset import (
 import wandb
 import argparse
 import pylab as pl
-from uni_prot.dense_model import DenseModel
-from uni_prot.uni_prot_dataset import UniProtDataset
+import os 
 
 cudnn.benchmark = True
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -34,7 +33,7 @@ torch.cuda.list_gpu_processes()
 from util.train_helper import train_model
 from datetime import datetime as dt
 from util.experiments import store_experiment
-
+from thermostability.uni_prot_dataset import UniProtDataset
 
 def run_train_experiment(results_path, config: dict = None, use_wandb=True):
     representation_key = config["representation_key"]
@@ -79,7 +78,6 @@ def run_train_experiment(results_path, config: dict = None, use_wandb=True):
             # collate_fn=zero_padding700_collate,
         ),
     }
-    dataset_sizes = {"train": len(train_ds), "val": len(eval_ds)}
     input_sizes = {
         "esm_s_B_avg": 2560,
         "uni_prot": 1024,
@@ -89,15 +87,11 @@ def run_train_experiment(results_path, config: dict = None, use_wandb=True):
 
     input_size = input_sizes[representation_key]
     thermo = (
-        DenseModel(
-            layers=config["model_hidden_layers"],
-            dropout_rate=config["model_dropoutrate"],
-        )
-        if config["model"] == "uni_prot"
-        else HotInferPregeneratedFC(
+        HotInferPregeneratedFC(
             input_len=input_size,
             num_hidden_layers=config["model_hidden_layers"],
             first_hidden_size=config["model_first_hidden_units"],
+            p_dropout=config["model_dropoutrate"]
         )
         if config["model"] == "fc"
         else CNNPregeneratedFC(
@@ -132,20 +126,20 @@ def run_train_experiment(results_path, config: dict = None, use_wandb=True):
         )
     )
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.5)
-    model, best_epoch_loss, best_epoch_mad, epoch_losses,best_epoch_actuals, best_epoch_predictions = train_model(
+    if not use_wandb:
+        os.makedirs(results_path, exist_ok=True)
+    model, best_epoch_loss, best_epoch_mad, epoch_mads,best_epoch_actuals, best_epoch_predictions = train_model(
         model,
         criterion,
         exp_lr_scheduler,
         dataloaders,
-        dataset_sizes,
         use_wandb,
         num_epochs=config["epochs"],
         prepare_inputs=lambda x: x.to("cuda:0"),
         prepare_labels=lambda x: x.to("cuda:0")
         if not model_parallel
         else x.to("cuda:1"),
-        label=representation_key,
-        best_model_path=results_path,
+        best_model_path = os.path.join(results_path, "model.pt") if not use_wandb else None,
     )
     if use_wandb:
         data = [
@@ -158,7 +152,7 @@ def run_train_experiment(results_path, config: dict = None, use_wandb=True):
         table = wandb.Table(data=data, columns=["predictions", "labels"])
         wandb.log({"predictions": wandb.plot.scatter(table, "predictions", "labels")})
     else:
-        store_experiment(results_path,best_epoch_loss, best_epoch_mad, best_epoch_predictions, best_epoch_actuals, config, epoch_losses)
+        store_experiment(results_path,best_epoch_loss, best_epoch_mad, best_epoch_predictions, best_epoch_actuals, config, epoch_mads)
 
     return best_epoch_loss
 
@@ -171,22 +165,21 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--val_on_trainset", type=str, choices=["true", "false"])
     parser.add_argument("--dataset_limit", type=int, default=1000000)
-    parser.add_argument("--optimizer", type=str, default="adam")
+    parser.add_argument("--optimizer", type=str, default="adam", choices=["adam", "sgd"])
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--model", type=str, default="uni_prot")
-
+    parser.add_argument("--model", type=str, default="fc", choices=["fc", "cnn"])
     parser.add_argument("--model_parallel", type=str, choices=["true", "false"])
-    parser.add_argument("--no_wandb", action="store_true")
+    parser.add_argument("--wandb", action="store_true")
     parser.add_argument("--representation_key", type=str, default="uni_prot")
     parser.add_argument("--model_dropoutrate", type=float, default=0.3)
     parser.add_argument("--weight_regularizer", type=bool, default=True)
-    parser.add_argument("--seq_length", type=int, default=100000)
+    parser.add_argument("--seq_length", type=int, default=700)
 
     args = parser.parse_args()
 
     argsDict = vars(args)
-    use_wandb = not argsDict["no_wandb"]
-    del argsDict["no_wandb"]
+    use_wandb = argsDict["wandb"]
+    del argsDict["wandb"]
     representation_key = argsDict["representation_key"]
     currentTime = dt.now().strftime("%d-%m-%y_%H:%M:%S")
     results_path = f"results/train/{representation_key}/{currentTime}"
