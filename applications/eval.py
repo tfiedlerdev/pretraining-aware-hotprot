d47baacf338104577ab2bc4051d6b1c696358fa2
tfiedlerdev <tobias.fiedler@student.hpi.de>
2023-02-17 15:44:49 +0100

    Rename train dir to applications dir

---
+++ applications/eval.py
@@ -0,0 +1,81 @@
+import torch
+from torch.utils.data import DataLoader
+from torch import nn as nn
+import torch.backends.cudnn as cudnn
+from thermostability.thermo_pregenerated_dataset import (
+    ThermostabilityPregeneratedDataset,
+)
+import argparse
+from util.experiments import store_experiment
+cudnn.benchmark = True
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if torch.cuda.is_available():
+    torch.cuda.empty_cache()
+
+cpu = torch.device("cpu")
+torch.cuda.empty_cache()
+torch.cuda.list_gpu_processes()
+from tqdm.notebook import tqdm
+from util.train_helper import execute_epoch
+from datetime import datetime as dt
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", "-m", type=str)
+    parser.add_argument("--batch_size", "-b", type=int, default=32)
+    parser.add_argument(
+        "--limit",
+        "-l",
+        type=int,
+        default=1000000,
+        help="Maximum number of samples to run evaluation on",
+    )
+    parser.add_argument(
+        "--dataset",
+        "-d",
+        type=str,
+        default="data/val.csv",
+        help="Path to dataset csv with format: sequence, melting point",
+    )
+    currentTime = dt.now().strftime("%d-%m-%y_%H:%M:%S")
+    parser.add_argument(
+        "--output_dir",
+        "-o",
+        type=str,
+        default=f"results/eval/{currentTime}",
+        help="Path to directory where evaluation plots will be saved in",
+    )
+    args = parser.parse_args()
+    argsDict = vars(args) 
+    
+    limit = argsDict["limit"]
+    model = torch.load(argsDict["model"]).to(device)
+    dsPath = argsDict["dataset"]
+    ds = ThermostabilityPregeneratedDataset(
+        dsPath, limit=limit, usePerProteinRep=True
+    )
+    dataloader = DataLoader(ds, argsDict["batch_size"], shuffle=False)
+    with torch.no_grad():
+        epoch_loss, epoch_mad, epoch_actuals, epoch_predictions = execute_epoch(
+            model,
+            nn.MSELoss(),
+            dataloader,
+            prepare_inputs=lambda x: x.to(device),
+            prepare_labels=lambda x: x.to(device),
+            on_batch_done=lambda idx, _, batch_loss, running_mad: tqdm.write(
+                "Batch: [{}/{}], batch loss: {:.6f}, epoch abs diff mean {:.6f}".format(
+                    idx + 1,
+                    len(dataloader),
+                    batch_loss,
+                    running_mad,
+                ),
+                end="\r",
+            ),
+        )
+ 
+    print(f"Evaluation done with \n- total average loss {epoch_loss:.2f}\n- total mean absolute difference {epoch_mad:.2f}")
+    output_dir_path = argsDict["output_dir"]
+
+    store_experiment(output_dir_path,epoch_loss, epoch_mad, epoch_predictions, epoch_actuals, args)
+   
--- applications/train.py
+++ applications/train.py
@@ -32,34 +32,36 @@ cpu = torch.device("cpu")
 torch.cuda.empty_cache()
 torch.cuda.list_gpu_processes()
 from util.train_helper import train_model
+from datetime import datetime as dt
+from util.experiments import store_experiment
 
 
-def run_train_experiment(config: dict = None, use_wandb=True):
+def run_train_experiment(results_path, config: dict = None, use_wandb=True):
     representation_key = config["representation_key"]
-    model_parallel = config["model_parallel"] =="true"
-    val_on_trainset = config["val_on_trainset"] =="true"
+    model_parallel = config["model_parallel"] == "true"
+    val_on_trainset = config["val_on_trainset"] == "true"
     limit = config["dataset_limit"]
-    seq_length= config["seq_length"]
+    seq_length = config["seq_length"]
     train_ds = (
-        UniProtDataset("train.csv", limit=limit, seq_length=seq_length)
+        UniProtDataset("data/train.csv", limit=limit, seq_length=seq_length)
         if representation_key == "uni_prot"
-        else ThermostabilityPregeneratedDataset("train.csv", limit=limit, usePerProteinRep=True) if representation_key=="s_s_0_avg"
-        else ThermostabilityDataset("train.csv", limit=limit)   )
-    
-    valFileName =  "train.csv" if val_on_trainset else "val.csv"
-    print("valFileName",valFileName)
+        else ThermostabilityPregeneratedDataset(
+            "data/train.csv", limit=limit, usePerProteinRep=True
+        )
+        if representation_key == "s_s_0_avg"
+        else ThermostabilityDataset("data/train.csv", limit=limit)
+    )
+
+    valFileName = "data/train.csv" if val_on_trainset else "data/val.csv"
+    print("valFileName", valFileName)
     eval_ds = (
-        UniProtDataset(
-           valFileName,
-            limit=limit,
-            seq_length=seq_length
-        )
+        UniProtDataset(valFileName, limit=limit, seq_length=seq_length)
         if representation_key == "uni_prot"
-        else ThermostabilityPregeneratedDataset(valFileName, limit=limit, usePerProteinRep=True) if representation_key=="s_s_0_avg"
-        else ThermostabilityDataset(
-            valFileName,
-           limit=limit
+        else ThermostabilityPregeneratedDataset(
+            valFileName, limit=limit, usePerProteinRep=True
         )
+        if representation_key == "s_s_0_avg"
+        else ThermostabilityDataset(valFileName, limit=limit)
     )
     dataloaders = {
         "train": DataLoader(
@@ -78,13 +80,18 @@ def run_train_experiment(config: dict = None, use_wandb=True):
         ),
     }
     dataset_sizes = {"train": len(train_ds), "val": len(eval_ds)}
-    input_sizes = {"esm_s_B_avg": 2560, "uni_prot": 1024, "s_s_0_A": 148 * 1024, "s_s_0_avg": 1024}
+    input_sizes = {
+        "esm_s_B_avg": 2560,
+        "uni_prot": 1024,
+        "s_s_0_A": 148 * 1024,
+        "s_s_0_avg": 1024,
+    }
 
     input_size = input_sizes[representation_key]
     thermo = (
         DenseModel(
-            layers=config['model_hidden_layers'],
-            dropout_rate=config['model_dropoutrate']
+            layers=config["model_hidden_layers"],
+            dropout_rate=config["model_dropoutrate"],
         )
         if config["model"] == "uni_prot"
         else HotInferPregeneratedFC(
@@ -111,20 +118,21 @@ def run_train_experiment(config: dict = None, use_wandb=True):
     if use_wandb:
         wandb.watch(thermo)
     criterion = nn.MSELoss()
-    weight_decay = 1e-5 if config['weight_regularizer'] else 0
+    weight_decay = 1e-5 if config["weight_regularizer"] else 0
     optimizer_ft = (
         torch.optim.Adam(
-            thermo.parameters(), 
-            lr=config["learning_rate"], 
-            weight_decay= weight_decay
-            )
+            thermo.parameters(), lr=config["learning_rate"], weight_decay=weight_decay
+        )
         if config["optimizer"] == "adam"
         else torch.optim.SGD(
-            thermo.parameters(), lr=config["learning_rate"], momentum=0.9, weight_decay=weight_decay,
+            thermo.parameters(),
+            lr=config["learning_rate"],
+            momentum=0.9,
+            weight_decay=weight_decay,
         )
     )
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.5)
-    thermo, score = train_model(
+    model, best_epoch_loss, best_epoch_mad, epoch_losses,best_epoch_actuals, best_epoch_predictions = train_model(
         model,
         criterion,
         exp_lr_scheduler,
@@ -133,11 +141,26 @@ def run_train_experiment(config: dict = None, use_wandb=True):
         use_wandb,
         num_epochs=config["epochs"],
         prepare_inputs=lambda x: x.to("cuda:0"),
-        prepare_labels=lambda x: x.to("cuda:0") if not model_parallel else x.to("cuda:1"),
+        prepare_labels=lambda x: x.to("cuda:0")
+        if not model_parallel
+        else x.to("cuda:1"),
         label=representation_key,
+        best_model_path=results_path,
     )
-    
-    return score
+    if use_wandb:
+        data = [
+            [x, y]
+            for (x, y) in zip(
+                best_epoch_predictions,
+                best_epoch_actuals,
+            )
+        ]
+        table = wandb.Table(data=data, columns=["predictions", "labels"])
+        wandb.log({"predictions": wandb.plot.scatter(table, "predictions", "labels")})
+    else:
+        store_experiment(results_path,best_epoch_loss, best_epoch_mad, best_epoch_predictions, best_epoch_actuals, config, epoch_losses)
+
+    return best_epoch_loss
 
 
 if __name__ == "__main__":
@@ -148,24 +171,34 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--val_on_trainset", type=str, choices=["true", "false"])
     parser.add_argument("--dataset_limit", type=int, default=1000000)
-    parser.add_argument("--optimizer", type=str,default="adam")
+    parser.add_argument("--optimizer", type=str, default="adam")
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--model", type=str, default="uni_prot")
-    
+
     parser.add_argument("--model_parallel", type=str, choices=["true", "false"])
     parser.add_argument("--no_wandb", action="store_true")
     parser.add_argument("--representation_key", type=str, default="uni_prot")
     parser.add_argument("--model_dropoutrate", type=float, default=0.3)
     parser.add_argument("--weight_regularizer", type=bool, default=True)
     parser.add_argument("--seq_length", type=int, default=100000)
+
     args = parser.parse_args()
 
     argsDict = vars(args)
     use_wandb = not argsDict["no_wandb"]
     del argsDict["no_wandb"]
+    representation_key = argsDict["representation_key"]
+    currentTime = dt.now().strftime("%d-%m-%y_%H:%M:%S")
+    results_path = f"results/train/{representation_key}/{currentTime}"
+    
+
     if use_wandb:
         with wandb.init(config=argsDict):
-            run_train_experiment(config=wandb.config, use_wandb=True)
+            run_train_experiment(
+                config=wandb.config, use_wandb=True, results_path=results_path
+            )
 
     else:
-        run_train_experiment(config=argsDict, use_wandb=False)
+        run_train_experiment(
+            config=argsDict, use_wandb=False, results_path=results_path
+        )
