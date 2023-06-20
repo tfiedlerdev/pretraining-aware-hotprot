c47d5feb59807d6d50953d738d68f956d0738fe2
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2023-06-20 17:05:37 +0200

    Move sbatch script

--- sbatch/train.sh
+++ sbatch/train.sh
@@ -4,11 +4,11 @@
 #SBATCH --gpus=1
 #SBATCH --mail-type ALL
 #SBATCH --mail-user tobias.fiedler@student.hpi.de
-#SBATCH --time=100:0:0
+#SBATCH --time=120:0:0
 
 eval "$(conda shell.bash hook)"
 conda activate hotprot
 export PYTHONPATH="/hpi/fs00/home/tobias.fiedler/hot-prot"
 export LD_LIBRARY_PATH="/hpi/fs00/home/tobias.fiedler/miniconda3/envs/hotprot/lib"
-srun wandb agent hotprot/hot-prot-applications/ix4guatr
+srun wandb "agent hotprot/hot-prot-applications/$1"
 
