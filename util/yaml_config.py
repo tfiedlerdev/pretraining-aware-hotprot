dd091f7967d66413b3701d759a4f5cf5468bab1a
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2023-08-18 09:48:19 +0000

    Add help

--- util/yaml_config.py
+++ util/yaml_config.py
@@ -7,6 +7,7 @@ help_text = {
     "WandBApiKey": "Your Weights and Biases API key. You can find it in your W&B account settings.",
     "HuggESMCacheDir": "Directory to store cached representations with format /<cache_dir>/start_token_{model_size}/<seq>.pt and /<cache_dir>/start_token_{model_size}/sequences.csv. For hugg_esm model, representation key is inferred from model size",
     "DatasetSplitsPath": "Path to directory containing the dataset split CSV files (train.csv, val.csv, test.csv, train_median.csv, val_median.csv, test_median.csv, train_FLIP.csv, val_FLIP.csv, test_FLIP.csv).",
+    "DatasetPath": "Path to directory containing the pregenerated dataset files with format /<dataset_path>/<representation_key>/<seq_index>.pt and /<dataset_path>/<representation_key>/sequences.csv.",
 }
 
 
@@ -29,7 +30,7 @@ class YamlConfig:
     def __getitem__(self, key):
         if self.config is None:
             self.config = self._load_config()
-        value = self.config[key]
+        value = self.config.get(key, default_value)
         if value == default_value:
             explanation = help_text.get(key, "")
             explanation = f"({explanation})" if explanation != "" else explanation
